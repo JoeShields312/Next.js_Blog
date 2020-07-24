94ece24429cc0bc57d02644724aec2fbb08ffa89
Joe Shields <shields.john.joseph@gmail.com>
2020-07-23 23:31:35 -0500

    updated main page

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,22 +1,36 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[
-Software Engineer skilled in both back-end and front-end, with particular interest in design and UX-UI experience. My career as a director of market research inspired me to pursue my true passion of creating web applications. I love being creative and unique, but I also appreciate the technical challenges of back-end development. When programming I follow structure and order, since this is essential when creating quality web applications. I enjoy debugging and critiquing small details, as well as optimizing applications. Working in a team setting is ideal, because more knowledge is obtained and retained between developers. In the next phase of my career, I want to continue to be challenged and tested as a means to maintain growth as a developer. In my off time I enjoy going to sporting and music events, drone piloting, fishing, cycling, and weight lifting. My past times, and my canine companions Belle and Scout keep me active between going on walks and frequenting the dog park. They even try their paws in development by nudging me as a reminder to take a step back and clear my head to remain effective.]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  )
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
 
+export default function Home({ allPostsData }) {
+  return (
+<Layout home>
+  <Head>...</Head>
+  <section className={utilStyles.headinMd}>...</section>
+  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+    <h2 className={utilStyles.headingLg}>Blog</h2>
+    <ul className={utilStyles.list}>
+      {allPostsData.map(({ id, date, title }) => (
+        <li className={utilStyles.listItem} key={id}>
+          {title}
+          <br />
+          {id}
+          <br />
+          {date}
+        </li>
+      ))}
+    </ul>
+  </section>
+</Layout>
+  )
+}
